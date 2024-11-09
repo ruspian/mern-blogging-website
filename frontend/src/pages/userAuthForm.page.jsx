cd69d8d04e911f17c6e3e9786c9e1be25b0107f3
ruspian <ruspianntb@gmail.com>
2024-11-10 01:07:47 +0800

    buat animasi untuk halaman login

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,82 +1,85 @@
 import { Link } from "react-router-dom";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className="h-cover flex items-center justify-center">
-      <form action="" className="w-[80%] max-w-[400px]">
-        <h1 className="text-3xl font-gelasio capitalize text-center mb-24">
-          {type == "sign-in" ? "Selemat Datang Kembali!" : "Gabung Sekarang!"}
-        </h1>
+    <AnimationWrapper keyValue={type}>
+      <section className="h-cover flex items-center justify-center">
+        <form action="" className="w-[80%] max-w-[400px]">
+          <h1 className="text-3xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Selemat Datang Kembali!" : "Gabung Sekarang!"}
+          </h1>
 
-        {/* input nama lengkap */}
-        {type !== "sign-in" ? (
+          {/* input nama lengkap */}
+          {type !== "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Nama Lengkap"
+              icon="fi-br-user"
+            />
+          ) : (
+            ""
+          )}
+
+          {/* input email */}
+
           <InputBox
-            name="fullname"
-            type="text"
-            placeholder="Nama Lengkap"
-            icon="fi-br-user"
+            name="email"
+            type="email"
+            placeholder="Email"
+            icon="fi-br-envelope"
           />
-        ) : (
-          ""
-        )}
 
-        {/* input email */}
+          {/* input password */}
 
-        <InputBox
-          name="email"
-          type="email"
-          placeholder="Email"
-          icon="fi-br-envelope"
-        />
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon="fi-br-lock"
+          />
 
-        {/* input password */}
+          {/* tombol daftar */}
+          <button className="btn-dark center mt-14" type="submit">
+            {type == "sign-in" ? "Masuk" : "Daftar"}
+          </button>
 
-        <InputBox
-          name="password"
-          type="password"
-          placeholder="Password"
-          icon="fi-br-lock"
-        />
+          <div
+            className="relative w-full flex items-center gap-2 my-10 opacity-20 uppercase text-black font-bold"
+            type="submit"
+          >
+            <hr className="w-1/2 border-b border-black" />
+            <p>atau</p>
+            <hr className="w-1/2 border-b border-black" />
+          </div>
 
-        {/* tombol daftar */}
-        <button className="btn-dark center mt-14" type="submit">
-          {type == "sign-in" ? "Masuk" : "Daftar"}
-        </button>
+          {/* tombol google */}
+          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+            <img src={googleIcon} className="w-5" alt="" />
+            Masuk dengan Google
+          </button>
 
-        <div
-          className="relative w-full flex items-center gap-2 my-10 opacity-20 uppercase text-black font-bold"
-          type="submit"
-        >
-          <hr className="w-1/2 border-b border-black" />
-          <p>atau</p>
-          <hr className="w-1/2 border-b border-black" />
-        </div>
-
-        {/* tombol google */}
-        <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-          <img src={googleIcon} className="w-5" alt="" />
-          Masuk dengan Google
-        </button>
-
-        {type == "sign-in" ? (
-          <p className="mt-6 text-dark-grey text-xl text-center">
-            Belum Punya Akun?{" "}
-            <Link to="/signup" className="underline text-black text-xl ml-1">
-              Daftar Disini!
-            </Link>
-          </p>
-        ) : (
-          <p className="mt-6 text-dark-grey text-xl text-center">
-            Sudah Mempunyai Akun?{" "}
-            <Link to="/signin" className="underline text-black text-xl ml-1">
-              Masuk
-            </Link>
-          </p>
-        )}
-      </form>
-    </section>
+          {type == "sign-in" ? (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Belum Punya Akun?{" "}
+              <Link to="/signup" className="underline text-black text-xl ml-1">
+                Daftar Disini!
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-dark-grey text-xl text-center">
+              Sudah Mempunyai Akun?{" "}
+              <Link to="/signin" className="underline text-black text-xl ml-1">
+                Masuk
+              </Link>
+            </p>
+          )}
+        </form>
+      </section>
+    </AnimationWrapper>
   );
 };
 
