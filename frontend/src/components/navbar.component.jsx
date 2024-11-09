cd5380b64311171989d685057189b771c6257752
ruspian <ruspianntb@gmail.com>
2024-11-09 17:54:41 +0800

    membuat navbar

---
+++ frontend/src/components/navbar.component.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import logo from "../imgs/logo.png";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [boxPencarian, setBoxPencarian] = useState(false);
+
+  return (
+    // logo
+    <nav className="navbar">
+      <Link to="/" className="flex-none w-10">
+        <img src={logo} className="w-full" />
+      </Link>
+
+      {/* form pencarian */}
+      <div
+        className={
+          "absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " +
+          (boxPencarian ? "show" : "hide")
+        }
+      >
+        <input
+          type="text"
+          placeholder="Cari..."
+          className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full claceholder:text-dark-grey md:pl-12"
+        />
+        <i className="fi fi-br-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+      </div>
+
+      {/* tombol pencarian */}
+      <div className="flex items-center gap-3 md:gap-6 ml-auto">
+        <button
+          className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+          onClick={() => setBoxPencarian(!boxPencarian)}
+        >
+          <i className="fi fi-br-search text-xl"></i>
+        </button>
+
+        {/* tombol buat artikel */}
+        <Link to="/editor" className="hidden md:flex gap-2 link">
+          <i className="fi fi-rr-edit-alt"></i>
+          <p>Buat Artikel</p>
+        </Link>
+
+        {/* tombol masuk */}
+        <Link className="btn-dark py-2" to="signin">
+          Masuk
+        </Link>
+
+        {/* tombol daftar */}
+        <Link className="btn-light py-2 hidden md:block" to="signup">
+          Daftar
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
