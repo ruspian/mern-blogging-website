467c535a47a365da9d5d481f7504a078d678e925
ruspian <ruspianntb@gmail.com>
2024-11-09 22:20:08 +0800

    mengatur route navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="/signin" element={<h1>Halaman Sign in</h1>} />
+        <Route path="/signup" element={<h1>Halaman Sign up</h1>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
