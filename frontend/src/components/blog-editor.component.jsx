643bcdc7d415f70f3650ecc2c177d7b6eac12c2c
ruspian <ruspianntb@gmail.com>
2024-11-11 13:49:58 +0800

    buat nav editor component

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png";
+
 const EditorFormComponent = () => {
-  return <h1>ini adalah blog editor dari komponen </h1>;
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-10">
+          <img src={logo} alt="" />
+        </Link>
+
+        <p className="max-md:hidden trxt-black line-clamp-1 w-full">
+          Blog Baru
+        </p>
+
+        <div className="flex gap-4 ml-auto ">
+          <button className="btn-dark py-2">Terbitkan</button>
+          <button className="btn-light py-2">Simpan Draft</button>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default EditorFormComponent;
