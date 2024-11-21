05c49674ec01b56d36ad7639e05d52b35bcd0d7f
ruspian <ruspianntb@gmail.com>
2024-11-22 00:39:24 +0800

    ffetch atau mengambil data blog dari backend

--- frontend/src/pages/blog.page.jsx
+++ frontend/src/pages/blog.page.jsx
@@ -1,11 +1,53 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const blogDataStructure = {
+  title: "",
+  des: "",
+  content: [],
+  tags: [],
+  author: {
+    personal_info: {},
+  },
+  banner: "",
+  publishedAt: "",
+};
 
 const BlogPage = () => {
   let { blog_id } = useParams();
 
+  let [blog, setBlog] = useState(blogDataStructure);
+
+  let {
+    title,
+    conten,
+    banner,
+    author: {
+      personal_info: { fullname, profile_img, username },
+    },
+    publishedAt,
+  } = blog;
+
+  const fetchBlog = () => {
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/blog", { blog_id })
+      .then(({ data: { blog } }) => {
+        setBlog(blog);
+        // console.log(blog);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchBlog();
+  }, []);
+
   return (
     <div>
-      <h1>Blog Page - {blog_id}</h1>
+      <h1>Blog Page - {blog.title}</h1>
     </div>
   );
 };
