2ed878a8652637483ebc255529cb153a8230c464
ruspian <ruspianntb@gmail.com>
2024-11-22 01:07:31 +0800

    membuat tampilan halaman blog page

---
+++ frontend/src/components/blog-interaction.component.jsx
@@ -0,0 +1,9 @@
+const BlogInteractionComponent = () => {
+  return (
+    <div>
+      <h1>Interaction component</h1>
+    </div>
+  );
+};
+
+export default BlogInteractionComponent;
--- frontend/src/pages/blog.page.jsx
+++ frontend/src/pages/blog.page.jsx
@@ -1,6 +1,10 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import AnimationWrapper from "../common/page-animation";
+import Loader from "../components/loader.component";
+import { tanggal } from "../common/date";
+import BlogInteractionComponent from "../components/blog-interaction.component";
 
 export const blogDataStructure = {
   title: "",
@@ -18,13 +22,14 @@ const BlogPage = () => {
   let { blog_id } = useParams();
 
   let [blog, setBlog] = useState(blogDataStructure);
+  let [loading, setLoading] = useState(true);
 
   let {
     title,
     conten,
     banner,
     author: {
-      personal_info: { fullname, profile_img, username },
+      personal_info: { fullname, profile_img, username: author_username },
     },
     publishedAt,
   } = blog;
@@ -34,10 +39,12 @@ const BlogPage = () => {
       .post(import.meta.env.VITE_SERVER_DOMAIN + "/blog", { blog_id })
       .then(({ data: { blog } }) => {
         setBlog(blog);
+        setLoading(false);
         // console.log(blog);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   };
 
@@ -46,9 +53,38 @@ const BlogPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Blog Page - {blog.title}</h1>
-    </div>
+    <AnimationWrapper transition={{ duration: 0.2 }}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="max-w-[900px] center py-10 max-lg:px-[5vw]">
+          <img src={banner} className="aspevt-video" />
+
+          <div className="mt-12">
+            <h2 className="">{title}</h2>
+
+            <div className="flex max-sm:flex-col justify-between my-8">
+              <div className="flex gap-5 items-start">
+                <img src={profile_img} className="w-12 h-12 rounded-full" />
+
+                <p className="capitalize">
+                  {fullname} <br />@
+                  <Link to={`/user/${author_username}`} className="underline">
+                    {author_username}
+                  </Link>
+                </p>
+              </div>
+
+              <p className="text-sm opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5 text-dark-grey">
+                Diunggah: {tanggal(publishedAt)}
+              </p>
+            </div>
+          </div>
+
+          <BlogInteractionComponent />
+        </div>
+      )}
+    </AnimationWrapper>
   );
 };
 
