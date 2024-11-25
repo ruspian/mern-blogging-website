66acb5a76f1b8b8fd58dec12c9ec2c9e9635c659
ruspian <ruspianntb@gmail.com>
2024-11-25 16:28:22 +0800

    membuat fitur sembunyikan komentar

--- frontend/src/components/comment-card.component.jsx
+++ frontend/src/components/comment-card.component.jsx
@@ -3,14 +3,44 @@ import { tanggal } from "../common/date";
 import { UserContext } from "../App";
 import toast from "react-hot-toast";
 import CommentsField from "./comment-field.component";
+import { BlogContext } from "../pages/blog.page";
 
 const CommentsCardComponent = ({ index, leftVal, commentData }) => {
 
     let { commented_by: { personal_info: { fullname, username, profile_img } }, commentedAt, comment, _id } = commentData;
 
+    let { blog, blog: { comments: { result: commentsArray } }, setBlog } = useContext(BlogContext);
+
     let { userAuth: { access_token } } = useContext(UserContext)
 
     const [isReplying, setIsReplying] = useState(false);
+
+
+    const removeCommentCards = (startingPoint) => {
+
+        if (commentsArray[startingPoint]) {
+            while (commentsArray[startingPoint].childrenLevel > commentData.childrenLevel) {
+                commentsArray.splice(startingPoint, 1);
+
+                if (!commentsArray[startingPoint]) {
+                    break;
+                }
+            }
+
+
+        }
+
+        setBlog({ ...blog, comments: { result: commentsArray } })
+
+    }
+
+
+    const handleHideComments = () => {
+        commentData.isReplyLoaded = false;
+
+        removeCommentCards(index + 1)
+    }
+
     const handleReply = () => {
         if (!access_token) {
             return toast.error("Silahkan login dahulu!");
@@ -19,6 +49,8 @@ const CommentsCardComponent = ({ index, leftVal, commentData }) => {
         setIsReplying(preVal => !preVal);
 
     }
+
+
 
     return (
 
@@ -33,13 +65,26 @@ const CommentsCardComponent = ({ index, leftVal, commentData }) => {
                 <p className="font-gelasio text-xl ml-3"> {comment} </p>
 
                 <div className="flex gap-5 items-center text-sm mt-5">
+
+                    {
+                        commentData.isReplyLoaded ?
+                            <button
+                                onClick={handleHideComments}
+                                className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2">
+                                <i className="fi fi-br-comment-alt-minus"></i>
+                                Sembunyikan
+                            </button>
+                            :
+                            ""
+                    }
+
                     <button className="underline" onClick={handleReply}>Balas</button>
                 </div>
 
                 {
                     isReplying ?
                         <div className="mt-8">
-                            <CommentsField action="reply" index={index} replyingTo={_id} setIsReplying={setIsReplying} />
+                            <CommentsField action="Balas" index={index} replyingTo={_id} setIsReplying={setIsReplying} />
                         </div>
                         :
                         ""
