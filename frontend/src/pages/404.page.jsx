71333d16ada520135b1f69480f229fe4c3debd44
ruspian <ruspianntb@gmail.com>
2024-12-02 17:59:19 +0800

    perbaiki halaman 404

--- frontend/src/pages/404.page.jsx
+++ frontend/src/pages/404.page.jsx
@@ -7,7 +7,7 @@ const PageNotFound = () => {
     <section className="h-cover relative p-10 flex flex-col items-center gap-20 text-center">
       <img
         src={pageNotFoundImage}
-        className="select-none border-2 border-grey w-72 aspect-square object-cover rounded"
+        className="select-none w-[400px] h-[200px] aspect-square object-cover rounded"
       />
 
       <h1 className="text-3xl font-gelasio leading-7 ">
